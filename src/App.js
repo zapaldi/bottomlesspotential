f8bab511518b0bdf34cd500950f74e1e8c19bcb2
EC2 Default User <ec2-user@ip-172-31-36-27.us-west-2.compute.internal>
2021-03-27 17:30:54 +0000

    adding code

--- src/App.js
+++ src/App.js
@@ -3,11 +3,38 @@ import './App.css';
 import TodoCategory from './TodoCategory/todoCategory'
 
 class App extends React.Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    }
+    
+  }
+  
   render() {
     return ( 
-       <div> <TodoCategory></TodoCategory> </div> 
+       <div> 
+        {
+            4 * 8
+        }
+       </div> 
     );
   }
+  
+  componentDidMount = () => {
+    const todos = localStorage.getItem('todos')
+    
+    if ( todos ) {
+        const savedTodos = JSON.parse ( todos )
+        this.setState ({ todos: savedTodos} )
+        console.log ( todos )  
+    } else {
+        console.log ("No todos")  
+    }
+    
+  }
+  
 }
 
 export default App;
