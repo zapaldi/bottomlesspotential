dc3024e117ccfa771ba66ae2d45163725016cf45
EC2 Default User <ec2-user@ip-172-31-36-27.us-west-2.compute.internal>
2021-03-27 16:33:57 +0000

    added TodoCategory

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoCategory from './TodoCategory/todoCategory'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Learning Step By Step, Practicing Patience and learning to Enjoy the Journey</p>
-      </header>
-    </div>
+    <TodoCategory></TodoCategory>
   );
 }
 
---
+++ src/TodoCategory/todoCategory.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class TodoCategory extends React.Component {
+    render() {
+            <div>Hello from TodoCategory</div>
+    }
+
+}
+
+export default TodoCategory;
